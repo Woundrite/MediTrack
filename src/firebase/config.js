3e994f0f43be296ea69d50e45641668335f23ee7
Woundrite <ndbendale2004@gmail.com>
2025-03-23 15:19:35 +0530

    fixed process error

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,18 +2,17 @@
 import { initializeApp } from "@firebase/app";
 import { getAuth, GoogleAuthProvider } from "@firebase/auth";
 import { getFirestore } from "@firebase/firestore";
-import 'dotenv/config';
+import dotenv from 'dotenv';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-config({ path: __dirname + '.env' })
 const firebaseConfig = {
-  apiKey: process.env.APIKEY_FIREBASE,
-  authDomain: process.env.AUTHDOMAIN_FIREBASE,
-  projectId: process.env.PROJECTID_FIREBASE,
-  storageBucket: process.env.STORAGEBUCKET_FIREBASE,
-  messagingSenderId: process.env.MESSAGINGSENDERID_FIREBASE,
-  appId: process.env.APPID_FIREBASE,
-  measurementId: process.env.MEASUREMENTID
+  apiKey: "AIzaSyA8NOCCrK-NCD8iX7gdNSl_ptt0gbOn-TU",
+  authDomain: "meditrack-8dc73.firebaseapp.com",
+  projectId: "meditrack-8dc73",
+  storageBucket: "meditrack-8dc73.firebasestorage.app",
+  messagingSenderId: "284008071967",
+  appId: "1:284008071967:web:5c49e704eb942553f8a62a",
+  measurementId: "G-K2E0RQJRJB"
 };
 
 // Initialize Firebase
